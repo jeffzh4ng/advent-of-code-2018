6e5f442349f5dfdca48aa9a0e1ce2691d01a2048
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-09-17 11:30:14 -0400

    Finish aoc01 part 2

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,21 +1,44 @@
+use std::collections::HashSet;
+use std::error::Error;
 use std::fs;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     const FILE_NAME: &str = "frequencies.txt";
 
-    let mut count = 0;
     let contents = fs::read_to_string(FILE_NAME).unwrap();
+    part_one(&contents)?;
+    part_two(&contents)?;
+
+    Ok(())
+}
+
+fn part_one(contents: &str) -> Result<(), Box<dyn Error>> {
+    let mut freq = 0;
 
     for line in contents.lines() {
-        let sign = &line[0..1];
-        let mut number: i32 = line[1..].parse().unwrap();
-
-        if sign == "-" {
-            number *= -1;
-        }
-
-        count += number
+        let number: i32 = line.parse()?;
+        freq += number;
     }
 
-    println!("{}", count);
+    println!("{}", freq);
+    Ok(())
+}
+
+fn part_two(contents: &str) -> Result<(), Box<dyn Error>> {
+    let mut freq = 0;
+    let mut seen = HashSet::new();
+
+    loop {
+        for line in contents.lines() {
+            let number: i32 = line.parse()?;
+            freq += number;
+
+            if seen.contains(&freq) {
+                println!("{}", freq);
+                return Ok(());
+            }
+
+            seen.insert(freq);
+        }
+    }
 }
